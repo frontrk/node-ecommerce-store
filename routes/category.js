397eb52667d7453931e94d1b00a44225088ac6a7
radek <radekkolis@protonmail.com>
2020-11-09 15:29:49 +0100

    refactor controllers

--- routes/category.js
+++ routes/category.js
@@ -1,42 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const Category = require('../models/category')
+const categories = require('../controllers/category')
 
-router.get('/', async (req, res) => {
-    try {
-        const category = await Category.find()
-        res.json(category)
-    }catch(err) {
-        res.status(500).json({message: err.message})
-    }
-})
+router.route('/')
+    .get(categories.get_all_categories)
+    .post(categories.create_category)
 
-router.get('/:id',getCategory, (req, res) => {
-    res.json(req.body)
-})
+router.route('/:id')
+    .get(findCategory, categories.get_category)
+    .delete(findCategory, categories.delete_category)
 
-router.post('/', async (req, res) => {
-    const category = new Category({
-        name: req.body.name,
-      })
-      try {
-        const newCategory = await category.save()
-        res.status(201).json(newCategory)
-      } catch (err) {
-        res.status(400).json({ message: err.message })
-      }      
-})
-
-router.delete('/:id', getCategory, async (req, res) => {
-   try {
-        await res.category.remove()
-        res.json({ message: 'Deleted category' })
-   } catch (err) {
-       res.status(500).json({message: err.message})
-   }    
-})
-
-async function getCategory(req, res, next) {
+async function findCategory(req, res, next) {
     let category
     try {
         category = await Category.findById(req.params.id)
